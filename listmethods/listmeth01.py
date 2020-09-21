af86fb9331d616db10e0c4317d6d81cda2d783a2
Sanju Byanjankar <sanju_benz@outlook.com>
2020-09-21 20:10:34 +0000

    learning about dictionaries

--- listmethods/listmeth01.py
+++ listmethods/listmeth01.py
@@ -10,4 +10,3 @@ proto.extend(proto2) # pass proto2 as an argument to the extend method
 print(proto)
 protoa.append(proto2) # pass proto2 as an argument to the append method
 print(protoa)
-               
